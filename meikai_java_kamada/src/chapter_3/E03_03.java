a70f45dd417a7988b9be479f47f2010a929b3135
kayo kamada <ddt.kamada@gmail.com>
2019-10-20 15:45:07 +0900

    2019/10/20_演習3-3_1回目_進捗率100%

---
+++ meikai_java_kamada/src/chapter_3/E03_03.java
@@ -0,0 +1,28 @@
+/*
+ * List 3-5の最後のelseを、else if(n == 0)に変更したらどうなるかを検討せよ。
+ */
+package chapter_3;
+
+import java.util.Scanner;
+
+public class E03_03 {
+
+    public static void main(String[] args) {
+        // 標準入力ストリームから取り出す入力値を変数standardInputに格納する。
+        Scanner standardInput = new Scanner(System.in);
+        // 整数値の入力を促す。
+        System.out.print("整数値：");
+        // キーボードから読み込んだ整数値を変数numberに格納する。
+        int number = standardInput.nextInt();
+
+        if (number > 0)
+            // 変数numberが0より大きければ、値は正。
+            System.out.println("その値は正です。");
+        else if (number < 0)
+            // 変数numberが0未満であれば、値は負。
+            System.out.println("その値は負です。");
+        else if (number == 0)
+            // 変数numberが0より大きくなく、かつ0未満でもない値は0しかないので、この場合はelse と else if (number == 0)は同じ意味になる。
+            System.out.println("その値は0です。");
+    }
+}
