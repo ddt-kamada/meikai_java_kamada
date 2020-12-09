f9cfc28b60d2ef550b4e4d6a6f67c42a0fcadf91
kayo kamada <ddt.kamada@gmail.com>
2020-12-09 21:06:23 +0900

    [指摘箇所修正：演習 3_11]変数名修正

--- meikai_java_kamada/src/chapter_3/E03_15.java
+++ meikai_java_kamada/src/chapter_3/E03_15.java
@@ -11,25 +11,25 @@ public class E03_15 {
     public static void main(String[] args) {
         // 標準入力ストリームから取り出す入力値を変数standardInputに格納する。
         Scanner standardInput = new Scanner(System.in);
-        // 整数Aの入力を促す。
+        // 最初の整数の入力を促す。
         System.out.print("整数a：");
-        // 入力された整数値を変数integerAに格納する。
-        int integerA = standardInput.nextInt();
-        // 整数Bの入力を促す。
+        // 入力された整数値を変数integerFirstに格納する。
+        int integerFirst = standardInput.nextInt();
+        // 2番目の整数の入力を促す。
         System.out.print("整数b：");
-        // 入力された整数値を変数integerBに格納する。
-        int integerB = standardInput.nextInt();
+        // 入力された整数値を変数integerSecondに格納する。
+        int integerSecond = standardInput.nextInt();
 
-        // integerAがintegerBより小さければ値を交換
-        if (integerA < integerB) {
-            int temp = integerA;
-            integerA = integerB;
-            integerB = temp;
+        // 最初の入力値が2番目の入力値より小さければ値を交換
+        if (integerFirst < integerSecond) {
+            int tempInteger = integerFirst;
+            integerFirst = integerSecond;
+            integerSecond = tempInteger;
         }
         // 結果を出力
         System.out.println("a >= bとなるようにソートしました。");
-        System.out.println("変数aは" + integerA + "です。");
-        System.out.println("変数bは" + integerB + "です。");
+        System.out.println("変数aは" + integerFirst + "です。");
+        System.out.println("変数bは" + integerSecond + "です。");
 
     }
 
