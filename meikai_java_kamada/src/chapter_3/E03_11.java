4d51c4bc7d300c194368712e5c833bf59e7d913a
kayo kamada <ddt.kamada@gmail.com>
2020-12-09 21:04:54 +0900

    [指摘箇所修正：演習 3_11]画面にアウトプットされる文言を修正、重複している文言を変数にまとめる、判定のロジックをコメントに合わせる

--- meikai_java_kamada/src/chapter_3/E03_11.java
+++ meikai_java_kamada/src/chapter_3/E03_11.java
@@ -17,21 +17,25 @@ public class E03_11 {
         // 標準入力ストリームから取り出す入力値を変数standardInputに格納する。
         Scanner standardInput = new Scanner(System.in);
         // 最初の変数の入力を促す。
-        System.out.print("最初の整数を入力してください。：");
+        System.out.print("整数A：");
         // 入力された整数値を変数integerFirstに格納する。
         int integerFirst = standardInput.nextInt();
         // 次の整数値の入力を促す。
-        System.out.print("次の整数を入力してください。：");
+        System.out.print("整数B：");
         // 次に入力された整数値を変数integerSecondに格納する。
         int integerSecond = standardInput.nextInt();
+        // 入力された二つの整数を比べる際の基準値を変数に格納する。
+        int referenceValue = 11;
+        // 出力される答の中で、入力された二つの整数を比べると変化する値を変数に格納する。
+        String answerMoreThan = "11以上";
+        String answerLessThan = "10以下";
         /* 入力された二つの整数を比べて大きい方から小さい方を引いた差が、
-           11以上であれば文字列"11以上"を、10以下であれば文字列"10以下"を変数differenceFirstAndSecondに格納する。 */
-        String differenceFirstAndSecond = integerFirst > integerSecond
-                        ? ((integerFirst - integerSecond) > 10 ? "11以上" : "10以下")
-                        : ((integerSecond - integerFirst) > 10 ? "11以上" : "10以下");
-        // 変数differenceFirstAndSecondを出力する。
-        System.out.println("それらの差は" + differenceFirstAndSecond + "です。");
-
+        基準値以上であれば変数answerMoreThanを、基準値より小さければ変数answerLessThanを返して出力する。 */
+        System.out.println("それらの差は"
+                    + (integerFirst > integerSecond
+                            ? ((integerFirst - integerSecond) >= referenceValue ? answerMoreThan : answerLessThan)
+                            : ((integerSecond - integerFirst) >= referenceValue ? answerMoreThan : answerLessThan)
+                    ) + "です。");
     }
 
 }
