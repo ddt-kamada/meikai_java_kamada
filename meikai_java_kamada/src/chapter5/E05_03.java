295c8d548c461aa0070c9923c1b00c62a179eda1
kayo kamada <ddt.kamada@gmail.com>
2021-03-10 20:55:54 +0900

    2021/03/10_演習5-3_1回目_進捗率100%

---
+++ meikai_java_kamada/src/chapter5/E05_03.java
@@ -0,0 +1,22 @@
+/**
+ * 論理型の変数にtrueやfalseを代入して、その値を表示するプログラムを作成せよ。
+ *
+ */
+package chapter5;
+
+public class E05_03 {
+
+    public static void main(String[] args) {
+        // 論理型の変数を宣言し、trueを代入する。
+        boolean booleanTrue = true;
+        // 論理型の変数を宣言し、falseを代入する。
+        boolean booleanFlase = false;
+
+        // 論理型のtrueを代入された変数を出力する。
+        System.out.println("論理型のtrue：" + booleanTrue);
+        // 論理型のfalseを代入された変数を出力する。
+        System.out.println("論理型のfalse：" + booleanFlase);
+
+    }
+
+}
