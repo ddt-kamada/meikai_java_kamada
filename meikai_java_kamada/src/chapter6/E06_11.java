6c17a27da065ac524ac0783adb8249172e22bc2c
kayo kamada <ddt.kamada@gmail.com>
2021-06-14 21:10:19 +0900

    2021/06/14_演習6-11_指摘箇所修正_最初の1回だけ行う処理を繰り返し処理の中に入れない・for文の条件修正

--- meikai_java_kamada/src/chapter6/E06_11.java
+++ meikai_java_kamada/src/chapter6/E06_11.java
@@ -26,39 +26,31 @@ public class E06_11 {
 
         // 配列変数を宣言し、入力された素数のint型の配列を生成して初期化する。
         int[] intArray = new int[arrayNumber];
-        // 生成された配の要素に、繰り返し値を代入する。
-        for (int i=0; i<arrayNumber; i++) {
-            if (i == 0) {
-                // 配列の一つ目の要素では、生成した1〜10の乱数をそのまま代入する。
-                intArray[i] = randomGenerator.nextInt(10) + 1;
-            } else {
-                // 配列の要素が重複しているかのフラグを作成する。
-                boolean isDuplicated;
-                // 乱数を保持する変数を宣言する。
-                int randomValue;
-                // 重複フラグが真である間、繰り返し乱数の生成と重複チェック
-                do {
-                    // 乱数を保持する変数に、1〜10の乱数を生成して代入する。
-                    randomValue = randomGenerator.nextInt(10) + 1;
-                    // 重複フラグを偽にする。
-                    isDuplicated = false;
-                    // 配列を走査する。
-                    for (int j=0; i<arrayNumber; j++) {
-                        // 値を代入する前の配列要素の値は全て0なので、重複チェックを行わない。
-                        if (j > i) {
-                            break;
-                        }
-                        // 着目した要素の値と生成した乱数が等しければ、重複フラグを真にする。
-                        if (intArray[j] == randomValue) {
-                            isDuplicated = true;
-                            // 重複していることが分かったら繰り返しを終了する。
-                            break;
-                        }
+        // 配列の一つ目の要素では、生成した1〜10の乱数をそのまま代入する。
+        intArray[0] = randomGenerator.nextInt(10) + 1;
+        // 生成された配列の要素に、繰り返し値を代入する。
+        for (int i=1; i<arrayNumber; i++) {
+            // 配列の要素が重複しているかのフラグを作成する。
+            boolean isDuplicated;
+            // 乱数を保持する変数を宣言する。
+            int randomValue;
+            // 重複フラグが真である間、繰り返し乱数の生成と重複チェック
+            do {
+                isDuplicated = false;
+                // 乱数を保持する変数に、1〜10の乱数を生成して代入する。
+                randomValue = randomGenerator.nextInt(10) + 1;
+                // 配列を走査する。(代入前の値は全て0なので、代入済みの値のみ走査する。)
+                for (int j=0; j<i; j++) {
+                    // 着目した要素の値と生成した乱数が等しければ、重複フラグを真にする。
+                    if (intArray[j] == randomValue) {
+                        isDuplicated = true;
+                        // 重複していることが分かったら繰り返しを終了する。
+                        break;
                     }
-                } while (isDuplicated);
-                // 乱数を配列要素に代入する。
-                intArray[i] = randomValue;
-            }
+                }
+            } while (isDuplicated);
+            // 乱数を配列要素に代入する。
+            intArray[i] = randomValue;
         }
 
         // 確認のため、配列の各値を表示する。
